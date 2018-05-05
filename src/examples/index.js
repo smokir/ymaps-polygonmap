bd946dbb5948372d7264672ac54ab78100de5a29
Kirill Shatskiy <shaackiy@gmail.com>
2018-05-05 15:42:23 +0300

    feat: Update demo

--- src/examples/index.js
+++ src/examples/index.js
@@ -1,6 +1,6 @@
 import './../Polygonmap/Polygonmap';
 import points from './data/bikeparking-moscow.geojson';
-import polygons from './data/moscow-mo.geojson';
+import hexagonGrid from '../Polygonmap/utils/hexagonGrid';
 
 ymaps.ready(() => {
     const el = document.getElementById('map');
