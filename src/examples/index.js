bc24d924e4f67b8d414e7477bbdeaf0d020a644b
Ruslan Khusnetdinov <ruslankhh@gmail.com>
2018-05-05 18:48:35 +0300

    feat: Update styles for examples

--- src/examples/index.js
+++ src/examples/index.js
@@ -12,20 +12,15 @@ ymaps.ready(() => {
 
     const R = 20;
 
-    const map = new ymaps.Map(el, {
-        center: [55.76, 37.64],
-        zoom: 10
-    });
-
-    const zoom = map.getZoom();
+    const zoom = myMap.getZoom();
 
     const rect = el.getBoundingClientRect();
-    const center = map.getGlobalPixelCenter();
+    const center = myMap.getGlobalPixelCenter();
 
     const offsetLeft = center[0] - (rect.width / 2);
     const offsetTop = center[1] - (rect.height / 2);
 
-    const polygons = hexagonGrid(map, zoom, R, offsetLeft, offsetTop, rect.width, rect.height);
+    const polygons = hexagonGrid(myMap, zoom, R, offsetLeft, offsetTop, rect.width, rect.height);
 
     ymaps.modules.require(['Polygonmap'], (Polygonmap) => {
         const polygonmap = new Polygonmap({polygons, points});
