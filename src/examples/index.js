3b5f839a8f16ec24fdf3666d0db7f9c731da7ce6
Kirill Shatskiy <shaackiy@gmail.com>
2018-05-05 15:42:23 +0300

    feat: Update demo

--- src/examples/index.js
+++ src/examples/index.js
@@ -1,15 +1,32 @@
 import './../Polygonmap/Polygonmap';
 import points from './data/bikeparking-moscow.geojson';
-import polygons from './data/moscow-mo.geojson';
 import RangeMapper from '../Polygonmap/utils/rangeMapper';
+import hexagonGrid from '../Polygonmap/utils/hexagonGrid';
 
 ymaps.ready(() => {
-    // eslint-disable-next-line no-unused-vars
-    const myMap = new ymaps.Map('map', {
+    const el = document.getElementById('map');
+    const myMap = new ymaps.Map(el, {
         center: [55.76, 37.64],
         zoom: 10,
         controls: ['zoomControl', 'typeSelector']
     });
+
+    const R = 20;
+
+    const map = new ymaps.Map(el, {
+        center: [55.76, 37.64],
+        zoom: 10
+    });
+
+    const zoom = map.getZoom();
+
+    const rect = el.getBoundingClientRect();
+    const center = map.getGlobalPixelCenter();
+
+    const offsetLeft = center[0] - (rect.width / 2);
+    const offsetTop = center[1] - (rect.height / 2);
+
+    const polygons = hexagonGrid(map, zoom, R, offsetLeft, offsetTop, rect.width, rect.height);
 
     ymaps.modules.require(['Polygonmap'], (Polygonmap) => {
         const polygonmap = new Polygonmap({polygons, points});
