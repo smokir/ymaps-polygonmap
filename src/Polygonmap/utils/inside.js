dbc4ac07d76a3bebf484346879d3e69b385d242b
Ruslan Khusnetdinov <ruslankhh@gmail.com>
2018-05-05 15:53:47 +0300

    fix: Refactor inside function

--- src/Polygonmap/utils/inside.js
+++ src/Polygonmap/utils/inside.js
@@ -1,23 +1,17 @@
 import contains from 'robust-point-in-polygon';
 
 const inside = (polygon, point) => {
-    function isInside(polygonCoord, pointCoord) {
-        return contains(polygonCoord, pointCoord) !== 1;
-    }
-
     // TODO: Check format, is it GeoJSON
     const pointCoord = point.coordinates;
     const polygonsCoord = polygon.coordinates;
     const polygonCoord = polygonsCoord[0];
 
-    let result = false;
-
-    result = isInside(polygonCoord, pointCoord);
+    let result = contains(polygonCoord, pointCoord) !== 1;
 
     if (result) {
         for (let k = 1; k < polygonsCoord.length && !result; k++) {
             const holeCoord = polygonsCoord[k];
-            const isInsideHole = isInside(holeCoord, pointCoord);
+            const isInsideHole = contains(holeCoord, pointCoord) !== 1;
 
             if (isInsideHole) {
                 result = !isInsideHole;
@@ -26,7 +20,7 @@ const inside = (polygon, point) => {
     } else {
         for (let k = 1; k < polygonsCoord.length && result; k++) {
             const polygonCoord = polygonsCoord[k];
-            const isInsidePolygon = isInside(polygonCoord, pointCoord);
+            const isInsidePolygon = contains(polygonCoord, pointCoord) !== 1;
 
             if (isInsidePolygon) {
                 result = isInsidePolygon;
