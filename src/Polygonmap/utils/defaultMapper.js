4124ac572be86b32ae7cbf0aadfaed50c699fb2f
Ruslan Khusnetdinov <ruslankhh@gmail.com>
2018-05-05 19:10:48 +0300

    feat: Edit styles (hardcode)

--- src/Polygonmap/utils/defaultMapper.js
+++ src/Polygonmap/utils/defaultMapper.js
@@ -6,10 +6,32 @@ import Colorize from './colorize/index';
  */
 const defaultMapper = function (feature) {
     const colorize = new Colorize(this.pointsCountMaximum, this.options.get('color'));
-
-    feature.options = {
-        fillColor: colorize.getColor(feature.properties.pointsCount)
-    };
+    if (feature.properties.pointsCount > 0 && feature.properties.pointsCount < 100) {
+        feature.options = {
+            fillColor: colorize.getColor(feature.properties.pointsCount),
+            strokeColor: '999999'
+        };
+    } else if (feature.properties.pointsCount >= 160) {
+        feature.options = {
+            fillColor: colorize.getColor(99),
+            strokeColor: '999999'
+        };
+    } else if (feature.properties.pointsCount >= 120) {
+        feature.options = {
+            fillColor: colorize.getColor(70),
+            strokeColor: '999999'
+        };
+    } else if (feature.properties.pointsCount >= 100) {
+        feature.options = {
+            fillColor: colorize.getColor(60),
+            strokeColor: '999999'
+        };
+    } else {
+        feature.options = {
+            fillColor: 'aaaaaa00',
+            strokeColor: '999999'
+        };
+    }
 
     return feature;
 };
