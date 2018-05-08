fc8e5c5387e59b4fe750bdd8612bc5ca79ffddb7
Kirill Shatskiy <shaackiy@gmail.com>
2018-05-08 10:49:05 +0300

    bug: Remove the coordinates reversing.

--- src/Polygonmap/utils/normalizeFeature.js
+++ src/Polygonmap/utils/normalizeFeature.js
@@ -1,26 +1,10 @@
-import reverseCoordinates from './reverseCoordinates';
-
 const normalizeFeature = (feature, meta, props = {}) => {
-    let coordinates;
-    let type = feature.geometry.type;
+    let {type, coordinates} = feature.geometry;
 
     if (feature.geometry.type === 'MultiPolygon') {
         type = 'Polygon';
-        if (typeof meta !== 'undefined' && meta.coordinatesOrder === 'longlat') {
-            coordinates = feature.geometry.coordinates.reduce((acc, coordinates) => {
-                return acc.concat(coordinates);
-            }, []);
-        } else {
-            coordinates = feature.geometry.coordinates.reduce((acc, coordinates) => {
-                return acc.concat(reverseCoordinates(coordinates));
-            }, []);
-        }
-    } else {
-        if (typeof meta !== 'undefined' && meta.coordinatesOrder === 'longlat') {
-            coordinates = feature.geometry.coordinates;
-        } else {
-            coordinates = reverseCoordinates(feature.geometry.coordinates);
-        }
+        coordinates = feature.geometry.coordinates
+            .reduce((acc, coordinates) => acc.concat(coordinates), []);
     }
 
     return Object.assign({}, props, feature, {
