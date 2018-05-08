76b38267711f4e1aaa3194f005117e605748422e
Ruslan Khusnetdinov <ruslankhh@gmail.com>
2018-05-08 14:01:48 +0300

    test: Add test for normalizeFeature

---
+++ src/Polygonmap/utils/normalizeFeature.test.js
@@ -0,0 +1,90 @@
+import {describe, it} from 'mocha';
+import {expect} from 'chai';
+
+import normalizeFeature from './normalizeFeature';
+
+const points = {
+    type: 'FeatureCollection',
+    features: [
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [37.64, 55.76]
+            }
+        }
+    ]
+};
+
+const polygons = {
+    type: 'FeatureCollection',
+    features: [
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'MultiPolygon',
+                coordinates: [
+                    [
+                        [
+                            [37.60, 55.70],
+                            [37.70, 55.70],
+                            [37.70, 55.80],
+                            [37.60, 55.80]
+                        ],
+                        [
+                            [37.61, 55.72],
+                            [37.61, 55.72],
+                            [37.63, 55.74],
+                            [37.63, 55.74]
+                        ]
+                    ],
+                    [
+                        [
+                            [37.40, 55.70],
+                            [37.50, 55.70],
+                            [37.50, 55.80],
+                            [37.40, 55.80]
+                        ],
+                        [
+                            [37.41, 55.72],
+                            [37.41, 55.72],
+                            [37.53, 55.74],
+                            [37.53, 55.74]
+                        ]
+                    ]
+                ]
+            }
+        }
+    ]
+};
+
+describe('normalizeFeature', () => {
+    it('should convert MultiPolygon type of geometry to Polygon', () => {
+        const expectedType = 'Polygon';
+        const result = normalizeFeature(polygons.features[0]);
+
+        const geometry = result.geometry;
+
+        expect(geometry.type).to.be.equal(expectedType);
+        expect(geometry.coordinates).lengthOf(4);
+        expect(geometry.coordinates[0]).lengthOf(4);
+        expect(geometry.coordinates[0][0]).lengthOf(2);
+        expect(geometry.coordinates[0][0][0]).to.be.a('number');
+    });
+
+    it('should add a property fillRule and it should to be equal \'evenOdd\'', () => {
+        const expectedFillRule = 'evenOdd';
+        const result = normalizeFeature(polygons.features[0]);
+        const geometry = result.geometry;
+
+        expect(geometry.fillRule).to.be.equal(expectedFillRule);
+    });
+
+    it('should save type if it is not a MultiPolygon type of geometry', () => {
+        const expectedType = points.features[0].geometry.type;
+        const result = normalizeFeature(points.features[0]);
+        const geometry = result.geometry;
+
+        expect(geometry.type).to.be.equal(expectedType);
+    });
+});
