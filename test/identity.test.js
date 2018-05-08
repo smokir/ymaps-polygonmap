d80e970c64ed3f9f59082f2948e20e33af4a33d1
tokarev_denis <blok1993@yandex.ru>
2018-05-08 10:04:57 +0300

    fix: Resolving conflict with tests.

--- test/identity.test.js
+++
@@ -1,9 +0,0 @@
-import {describe, it} from 'mocha';
-import {expect} from 'chai';
-
-describe('identity', () => {
-    it('should work', () => {
-        const expected = 'It\'s work!';
-        expect(expected).to.be.equal(expected);
-    });
-});
