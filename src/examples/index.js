dd8c2b604783cafb991926f7267ca232d0770961
Ruslan Khusnetdinov <ruslankhh@gmail.com>
2018-05-11 13:16:04 +0300

    fix: Reverse points coordinate for examples

--- src/examples/index.js
+++ src/examples/index.js
@@ -1,39 +1,23 @@
 /* eslint-disable */
 
 import './../Polygonmap/Polygonmap';
+// import points from './data/bikeparking-moscow.geojson';
 import points from './data/pyaterochka-moscow.geojson';
-import pointss from './data/bikeparking-moscow.geojson';
-//import pointsP from './data/piaterochka-m.geojson';
 import polygons from './data/moscow-mo.geojson';
 
 ymaps.ready(() => {
     // eslint-disable-next-line no-unused-vars
     const myMap = new ymaps.Map('map', {
-        center: [55.76, 37.64],
+        center: [37.64, 55.76],
         zoom: 10,
         controls: ['zoomControl', 'typeSelector']
     });
 
-    console.log(points);
-
     points.features.forEach(el => {
-        console.log(el.geometry.coordinates);
-        //el.geometry.coordinates = el.geometry.coordinates.reverse(); //раскоментируй чтобы реверснуть
-        console.log(el.geometry.coordinates);
-
-        const myPlacemark = new ymaps.Placemark(el.geometry.coordinates, {
-            balloonContentHeader: "Балун метки",
-            balloonContentBody: `${el.properties.name}<br>${el.properties.description}`,
-            hintContent: `${el.properties.name}`
-        });
-
-        myMap.geoObjects.add(myPlacemark);
+        el.geometry.coordinates = el.geometry.coordinates.reverse();
     });
 
-    //console.log(points);
-
     ymaps.modules.require(['Polygonmap'], (Polygonmap) => {
-        //console.log(points);
         const polygonmap = new Polygonmap({polygons, points});
 
         polygonmap.setMap(myMap);
