02c752ac5d35a2261607167d02bfdc2115af7730
vit134 <vit134@mail.ru>
2018-05-19 14:36:34 +0300

    test: update tests of colorize

--- src/utils/colorize.test.js
+++ src/utils/colorize.test.js
@@ -36,6 +36,20 @@ describe('colorize', () => {
         });
     });
 
+    describe('check returned colorRanges', () => {
+        it('should return array', () => {
+            const colorize = new Colorize(maxPointsCount, defaultOptions);
+            const expected = colorize.getColorRanges();
+            assert.isArray(expected);
+        });
+
+        it('should return array of 10 elements', () => {
+            const colorize = new Colorize(maxPointsCount, defaultOptions);
+            const expected = colorize.getColorRanges();
+            assert.lengthOf(expected, 10);
+        });
+    });
+
     describe('check returned color value', () => {
         it('should return correct color for max value', () => {
             const colorize = new Colorize(maxPointsCount, defaultOptions);
