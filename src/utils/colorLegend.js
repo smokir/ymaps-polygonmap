fda6dc9f6cf9e3a1fe7320a9bf6142a6a063eeb1
vit134 <vit134@mail.ru>
2018-05-21 10:52:42 +0300

    fix: set correct value in legend

--- src/utils/colorLegend.js
+++ src/utils/colorLegend.js
@@ -17,7 +17,8 @@ const init = (polygonmap) => {
     const colors = colorScheme.map((el, i) => {
         return {
             name: el,
-            value: colorRanges[i]
+            value: colorRanges[i],
+            opacity: polygonmap.options.get('colorOpacity')
         };
     });
 
@@ -59,9 +60,9 @@ const defaultTemplate = (colors) => {
             ${colors.map((color, i) => `
                 <div class="legend__row">
                     <span class="legend__value">
-                        ${colors[i - 1] ? `${colors[i - 1].value} - ${color.value}` : `0 - ${color.value}`}
+                        ${colors[i - 1] ? `${colors[i - 1].value + 1} - ${color.value}` : `1 - ${color.value}`}
                     </span>
-                    <span class="legend__color" style="background: ${color.name};"></span>
+                    <span class="legend__color" style="background: ${color.name}; opacity: ${color.opacity}"></span>
                 </div>
             `).join('\n')}
         </div>
