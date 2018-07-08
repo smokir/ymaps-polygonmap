1fc76489c83be7a39c2cf24331995dcaee0ff420
Ruslan Khusnetdinov <ruslankhh@gmail.com>
2018-07-09 02:10:34 +0300

    feat(module): add fillColor, strokeColor options for hover, active

--- src/utils/defaultOnMouseEnter.js
+++ src/utils/defaultOnMouseEnter.js
@@ -8,10 +8,18 @@ const defaultOnMouseEnter = function (e) {
     const objId = e.get('objectId');
 
     if (this._prevObjectId !== objId) {
-        this.objectManager.objects.setObjectOptions(objId, {
+        const fillColor = this.options.get('fillColorHover');
+        const options = {
             fillOpacity: this.options.get('fillOpacityHover'),
+            strokeColor: this.options.get('strokeColorHover'),
             strokeWidth: this.options.get('strokeWidthHover')
-        });
+        };
+
+        if (fillColor) {
+            options.fillColor = fillColor;
+        }
+
+        this.objectManager.objects.setObjectOptions(objId, options);
     }
 };
 
